015649da5c68aeafb59a525f4b4af0a77589a62c
Scott Hopkins <scottyhops@gmail.com>
2026-04-09 13:30:09 -0700

    added + to build message

--- test_app.py
+++ test_app.py
@@ -1,4 +1,4 @@
 from app import build_message
 
 def test_build_message():
-    assert build_message() == "Scott gets an A for his Jenkins build!"
+    assert build_message() == "Scott gets an A+ for his Jenkins build!"
